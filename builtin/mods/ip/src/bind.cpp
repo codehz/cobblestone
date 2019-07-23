7d6bc7c7befa3176d055c6da815dedb180b6ea08
Code Hz <CodeHz@outlook.com>
2019-07-23 16:30:33 +0800

    fix extra-port

--- builtin/mods/ip/src/bind.cpp
+++ builtin/mods/ip/src/bind.cpp
@@ -15,9 +15,11 @@ struct PropertiesSettings {
   std::string const &getCustomProperty(std::string const &) const;
 
   std::string const &getServerIP() const { return getCustomProperty("server-ip"); }
+  short getServerPort() const;
   std::string const &getServerPortExtra() const { return getCustomProperty("server-port-extra"); }
 
   std::string const &getServerIPv6() const { return getCustomProperty("server-ipv6"); }
+  short getServerPortv6() const;
   std::string const &getServerPortv6Extra() const { return getCustomProperty("server-portv6-extra"); }
 };
 
@@ -32,13 +34,16 @@ THook(int, bind, int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
         .ai_socktype = SOCK_DGRAM,
     };
     addrinfo *list;
+    auto src_port = ((sockaddr_in *)addr)->sin_port;
+    if (settings->getServerPort() != htons(src_port)) src_port = 0;
+    Log::debug("ipv4", "%d -> %d (%d)", htons(((sockaddr_in *)addr)->sin_port), htons(src_port), settings->getServerPort());
     if (getaddrinfo(settings->getServerIP().c_str(), settings->getServerPortExtra().data(), &hints, &list) == 0) {
-      if (((sockaddr_in *)addr)->sin_port != 0)
-        ((sockaddr_in *)list->ai_addr)->sin_port = ((sockaddr_in *)addr)->sin_port;
+      if (src_port != 0)
+        ((sockaddr_in *)list->ai_addr)->sin_port = src_port;
       auto ret = original(sockfd, list->ai_addr, list->ai_addrlen);
       freeaddrinfo(list);
       return ret;
-    } else if (((sockaddr_in *)addr)->sin_port == 0) {
+    } else if (src_port == 0) {
       if (auto port = atoi(settings->getServerPortExtra().data()); port) {
         sockaddr_in temp;
         memcpy(&temp, addr, sizeof(sockaddr_in));
@@ -54,13 +59,16 @@ THook(int, bind, int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
         .ai_socktype = SOCK_DGRAM,
     };
     addrinfo *list;
+    auto src_port = ((sockaddr_in6 *)addr)->sin6_port;
+    if (settings->getServerPortv6() != htons(src_port)) src_port = 0;
+    Log::debug("ipv6", "%d -> %d (%d)", htons(((sockaddr_in6 *)addr)->sin6_port), htons(src_port), settings->getServerPortv6());
     if (getaddrinfo(settings->getServerIPv6().c_str(), settings->getServerPortv6Extra().data(), &hints, &list) == 0) {
-      if (((sockaddr_in6 *)addr)->sin6_port != 0)
-        ((sockaddr_in6 *)list->ai_addr)->sin6_port = ((sockaddr_in6 *)addr)->sin6_port;
+      if (src_port != 0)
+        ((sockaddr_in6 *)list->ai_addr)->sin6_port = src_port;
       auto ret = original(sockfd, list->ai_addr, list->ai_addrlen);
       freeaddrinfo(list);
       return ret;
-    } else if (((sockaddr_in6 *)addr)->sin6_port == 0) {
+    } else if (src_port == 0) {
       if (auto port = atoi(settings->getServerPortv6Extra().data()); port) {
         sockaddr_in6 temp;
         memcpy(&temp, addr, sizeof(sockaddr_in6));
