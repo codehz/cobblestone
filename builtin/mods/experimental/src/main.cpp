850368ecc864c87a946962c955b2c776d379fb43
Code Hz <CodeHz@outlook.com>
2019-07-23 13:38:52 +0800

    addded override-saved-settings and cheats

--- builtin/mods/experimental/src/main.cpp
+++ builtin/mods/experimental/src/main.cpp
@@ -1,3 +1,4 @@
+#include <minecraft/json.h>
 #include <modloader/hook.hpp>
 #include <modloader/log.hpp>
 #include <modloader/refs.hpp>
@@ -12,6 +13,8 @@ struct PropertiesSettings {
   static bool parseBoolValue(std::string const &);
 
   bool getExperimentalMode() const { return parseBoolValue(getCustomProperty("experimental-mode")); }
+
+  bool getOverrideSavedSettings() const { return parseBoolValue(getCustomProperty("override-saved-settings")); }
 };
 
 TClasslessInstanceHook(bool, _ZNK9LevelData30hasExperimentalGameplayEnabledEv) {
@@ -20,11 +23,20 @@ TClasslessInstanceHook(bool, _ZNK9LevelData30hasExperimentalGameplayEnabledEv) {
   return original(this);
 }
 
-TClasslessInstanceHook(bool, _ZNK13LevelSettings31shouldForceExperimentalGameplayEv) {
-  if (refs<PropertiesSettings>->getExperimentalMode())
-    return true;
+struct LevelSettings {
+  void setForceExperimentalGameplay(bool val);
+};
+
+TInstanceHook(bool, _ZNK13LevelSettings31shouldForceExperimentalGameplayEv, LevelSettings) {
+  if (refs<PropertiesSettings>->getExperimentalMode()) {
+    setForceExperimentalGameplay(true);
+  }
   return original(this);
 }
+
+TClasslessInstanceHook(bool, _ZNK13LevelSettings27shouldOverrideSavedSettingsEv) { return refs<PropertiesSettings>->getOverrideSavedSettings(); }
+
+TClasslessInstanceHook(void, _ZN19ResourcePackManager23setExperimentalGameplayEb, bool flag) { original(this, refs<PropertiesSettings>->getExperimentalMode() || flag); }
 
 TClasslessInstanceHook(bool, _ZNK19ResourcePackManager22isExperimentalGameplayEv) {
   if (refs<PropertiesSettings>->getExperimentalMode())
@@ -34,8 +46,15 @@ TClasslessInstanceHook(bool, _ZNK19ResourcePackManager22isExperimentalGameplayEv
 
 TClasslessInstanceHook(bool, _ZNK12PackInstance9isVanillaEv) {
   bool ret = original(this);
-  print_stacktrace();
   if (refs<PropertiesSettings>->getExperimentalMode())
     return true;
   return ret;
 }
+
+TClasslessInstanceHook(bool, _ZNK6Player16forceAllowEatingEv) { return true; }
+
+TClasslessInstanceHook(void *, _ZN8GameRule17setRequiresCheatsEb, bool flag) { return original(this, false); }
+
+TClasslessInstanceHook(void *, _ZN8GameRule17setAllowInCommandEb, bool flag) { return original(this, true); }
+
+TClasslessInstanceHook(bool, _ZNK15CommandRegistry21requiresCheatsEnabledERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE) { return false; }
