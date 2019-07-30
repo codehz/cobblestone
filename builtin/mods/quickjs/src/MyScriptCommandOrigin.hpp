29dafefeb3aead78200a23a2545658b5545a09f9
Code Hz <CodeHz@outlook.com>
2019-07-30 10:53:33 +0800

    fix permission

--- builtin/mods/quickjs/src/MyScriptCommandOrigin.hpp
+++ builtin/mods/quickjs/src/MyScriptCommandOrigin.hpp
@@ -7,9 +7,8 @@ class MyScriptCommandOrigin : public ScriptCommandOrigin {
   std::function<void(Json::Value &&)> callback;
 
 public:
-  INLINE MyScriptCommandOrigin(ServerLevel &level, ScriptEngine &engine, std::function<void(Json::Value &&)> callback)
-      : ScriptCommandOrigin(level, engine)
-      , callback(callback) {}
+  INLINE MyScriptCommandOrigin(ServerLevel &level, ScriptEngine &engine, std::function<void(Json::Value &&)> callback) : ScriptCommandOrigin(level, engine), callback(callback) {}
   inline virtual ~MyScriptCommandOrigin() {}
   inline virtual void handleCommandOutputCallback(Json::Value &&value) { callback(std::move(value)); }
+  inline virtual CommandPermissionLevel getPermissionsLevel() const override { return (CommandPermissionLevel)4; }
 };
