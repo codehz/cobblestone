b86e673db43afe0f375daac5c0d14b0b528b0483
Sysca11 <46832985+Sysca11@users.noreply.github.com>
2019-08-15 15:50:53 +0800

    Update ItemStack.h

--- custom/minecraft/item/ItemStack.h
+++ custom/minecraft/item/ItemStack.h
@@ -2,7 +2,7 @@
 
 #include <string>
 #include <vector>
-
+class Item;
 class ItemInstance;
 
 class ItemStack {
@@ -10,11 +10,17 @@ class ItemStack {
 
 public:
   ItemStack();
+  ItemStack(ItemStack const &);
   ItemStack(ItemInstance const &);
+  ItemStack(const Item&,int amo);
   ItemStack &operator=(ItemStack const &);
   std::vector<std::string> getCustomLore() const;
   bool isNull() const;
   void setCustomLore(std::vector<std::string> const &);
   operator bool(void) const;
   inline ~ItemStack() { asm("call _ZN9ItemStackD2Ev"); };
+  std::string toString() const;
+  bool operator!=(ItemStack const &) const;
+  bool operator==(ItemStack const &) const;
+  
 };
